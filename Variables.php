53eaf63897351ca6777a8571fadaec44d24a4816
Usamaa1 <43386888+Usamaa1@users.noreply.github.com>
2020-01-24 00:06:24 +0500

    Variables in Php
    In this simple variables and their logic was defined and performed.

---
+++ Variables.php
@@ -0,0 +1,7 @@
+<?php
+    $Usama = "My name is: ";
+    $name = "Usama";
+    $age1 = "and his age is: ";
+    $age = 20;
+    echo "<h2>" . "$Usama" . "$name" . "</h2>" . "<br>" . "$age1" . "$age";
+?>
